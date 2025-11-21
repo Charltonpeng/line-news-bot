64dfb34579c7b2ff82b05448e24e88e0e0c4de79
Charltonpeng <earthlingpeng@gmail.com>
2025-11-21 15:40:06 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,6 +10,10 @@ const config = {
 
 const app = express();
 
+app.get('/webhook', (req, res) => {
+  res.status(200).send('OK');
+});
+
 // LINE Webhook 入口
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
